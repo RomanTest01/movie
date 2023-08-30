77cc2bffc69fc8572ede80931d4fc8433d651096
Roman <borisenko0893@gmail.com>
2023-08-30 20:42:00 +0300

    add tests and formatting

--- src/test/java/ProductManagerTest.java
+++ src/test/java/ProductManagerTest.java
@@ -16,22 +16,23 @@ public class ProductManagerTest {
 
         Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
-    public void notMoreLimit() {
+    public void findNotMoreFilms() {
         ProductManager manager = new ProductManager(5);
         manager.saveMovies("Kino 1");
         manager.saveMovies("Kino 2");
+        manager.saveMovies("Kino 3");
 
-        String[] expected = {"Kino 1", "Kino 2"};
-        String[] actual = manager.findLastNotMoreLimit();
+        String[] expected = {"Kino 3", "Kino 2", "Kino 1"};
+        String[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
-
     }
 
     @Test
-    public void moreLimit(){
-        ProductManager manager= new ProductManager(5);
+    public void findMoreFilms() {
+        ProductManager manager = new ProductManager(5);
         manager.saveMovies("Kino 1");
         manager.saveMovies("Kino 2");
         manager.saveMovies("Kino 3");
@@ -39,14 +40,14 @@ public class ProductManagerTest {
         manager.saveMovies("Kino 5");
         manager.saveMovies("Kino 6");
 
-        String[] expected = {"Kino 1", "Kino 2","Kino 3", "Kino 4","Kino 5", "Kino 6"};
-        String[] actual = manager.findLastMoreLimit();
+        String[] expected = {"Kino 6", "Kino 5", "Kino 4", "Kino 3", "Kino 2"};
+        String[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
-
     }
+
     @Test
-    public void  equalsLimit(){
+    public void findFilmsLimit() {
         ProductManager manager = new ProductManager(5);
         manager.saveMovies("Kino 1");
         manager.saveMovies("Kino 2");
@@ -54,39 +55,31 @@ public class ProductManagerTest {
         manager.saveMovies("Kino 4");
         manager.saveMovies("Kino 5");
 
-        String[] expected = {"Kino 1", "Kino 2","Kino 3", "Kino 4","Kino 5",};
-        String [] actual = manager.findLastMoreLimit();
+
+        String[] expected = {"Kino 5", "Kino 4", "Kino 3", "Kino 2", "Kino 1"};
+        String[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void  equalsElseLimit() {
-        ProductManager manager = new ProductManager(5);
-        manager.saveMovies("Kino 1");
-        manager.saveMovies("Kino 2");
-        manager.saveMovies("Kino 3");
-        manager.saveMovies("Kino 4");
-        manager.saveMovies("Kino 5");
-
-        String[] expected = {"Kino 1", "Kino 2", "Kino 3", "Kino 4", "Kino 5",};
-        String[] actual = manager.findLastNotMoreLimit();
-
-        Assertions.assertArrayEquals(expected, actual);
-
-    }
-
-    @Test
-    public void allTurnFilms() {
+    public void findMoreFilmsLimitAvto() {
         ProductManager manager = new ProductManager();
         manager.saveMovies("Kino 1");
         manager.saveMovies("Kino 2");
         manager.saveMovies("Kino 3");
+        manager.saveMovies("Kino 4");
+        manager.saveMovies("Kino 5");
+        manager.saveMovies("Kino 6");
+        manager.saveMovies("Kino 7");
+        manager.saveMovies("Kino 8");
+        manager.saveMovies("Kino 9");
 
-        String[] expected = {"Kino 3", "Kino 2", "Kino 1"};
+        String[] expected = {"Kino 9", "Kino 8", "Kino 7", "Kino 6", "Kino 5"};
         String[] actual = manager.findLast();
-    }
 
+        Assertions.assertArrayEquals(expected, actual);
+    }
 
     @Test
     public void moreTurnFilms() {
@@ -103,6 +96,8 @@ public class ProductManagerTest {
 
         String[] expected = {"Kino 9", "Kino 8", "Kino 7", "Kino 6", "Kino 5", "Kino 4", "Kino 3", "Kino 2", "Kino 1"};
         String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
     }
 
 
